5b646e8fee11b795763adb2ded56e58494b53909
storku <36249562+storku@users.noreply.github.com>
2018-05-09 21:26:24 -0700

    Add newMEssage and createMessage events

--- server/server.js
+++ server/server.js
@@ -13,6 +13,22 @@ app.use(express.static(publicPath));
 io.on('connection', socket => {
   console.log('New user connection');
 
+  // socket.emit('newEmail', {
+  //   from: 'coolkid@coolkids.com',
+  //   text: 'Hello, how are you',
+  //   createdAt: 123
+  // });
+
+  socket.emit('newMessage', {
+    from: 'coolkid2',
+    text: 'hello hello',
+    createdAt: 456
+  });
+
+  socket.on('createMessage', message => {
+    console.log('createMessage', message);
+  });
+
   socket.on('disconnect', () => {
     console.log('Client disconnected');
   });
